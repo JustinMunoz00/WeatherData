c6e7053d9dffc84f45a7371cc575eca13689d29e
JustinMunoz00 <justinm880@nycstudents.net>
2025-02-13 16:49:24 +0000

    Started Methods

--- WeatherData.java
+++ WeatherData.java
@@ -7,16 +7,24 @@ public class WeatherData
     * Cleans the data by removing from temperatures all values that are less than
     * lower and all values that are greater than upper, as described in part (a)
     */
-    public void cleanData(double lower, double upper)
-    { /* to be implemented in part (a) */ }
+    public void cleanData(double lower, double upper){ 
+        for (int i = 0; i < temperatures.size(); i++){
+            if (temperatures.get(i) < lower || temperatures.get(i) > upper) temperatures.remove(i);
+            i--;
+        }
+    }
 
     /**
     * Returns the length of the longest heat wave found in temperatures, as described in
     * part (b)
     * Precondition: There is at least one heat wave in temperatures based on threshold.
     */
-    public int longestHeatWave(double threshold)
-    { /* to be implemented in part (b) */ }
+    public int longestHeatWave(double threshold){
+        return 3;
+    }
     
     // There may be instance variables, constructors, and methods that are not shown.
+    public WeatherData(double[] temps){
+        for (double t : temps) temperatures.add(t);
+    }
 }
